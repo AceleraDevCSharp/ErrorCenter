257531dbd8837bcb856329401edffe8551e08cc4
AlessandroMaia <ale.maia.alm@gmail.com>
2020-07-02 23:02:53 -0300

    Add service error list

---
+++ ErrorCenter/ErrorCenter.IServices/IErrorListingFilters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ErrorCenter.IServices
+{
+    public interface IErrorListingFilters<TDomain, TEnvironment, TOrder, TTypeSearch, TSearch>
+        where TDomain : class
+    {
+        List<TDomain> SelectAll();
+        List<TDomain> SelectAllOrderedBy(TOrder order);
+
+        List<TDomain> SelectByEnvironment(TEnvironment environment);
+        List<TDomain> SelectByEnvironmentOrderedBy(TEnvironment environment, TOrder order);
+
+        List<TDomain> SelectSearchBy (TTypeSearch typeSearch, TSearch search);
+        List<TDomain> SelectSearchByOrderBy (TTypeSearch typeSearch, TSearch search, TOrder order);
+    }
+}
---
+++ ErrorCenter/ErrorCenter.Services/ErrorListingFilters.cs
@@ -0,0 +1,49 @@
+﻿using ErrorCenter.Domain;
+using ErrorCenter.IServices;
+using ErrorCenter.Persistence.EF.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ErrorCenter.Services
+{
+    public class ErrorListingFilters : IErrorListingFilters<ErrorLog, string, string, string, string>
+    {
+        protected DbContext _context;
+
+        public ErrorListingFilters(ErrorCenterDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<ErrorLog> SelectAll()
+        {
+            return _context.Set<ErrorLog>().ToList();
+        }
+
+        public List<ErrorLog> SelectAllOrderedBy(string order)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<ErrorLog> SelectByEnvironment(string environment)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<ErrorLog> SelectByEnvironmentOrderedBy(string environment, string order)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<ErrorLog> SelectSearchBy(string typeSearch, string search)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<ErrorLog> SelectSearchByOrderBy(string typeSearch, string search, string order)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
