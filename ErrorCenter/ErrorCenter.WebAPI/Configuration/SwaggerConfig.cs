c2c6b841bee7d1a662d41d2de2d83d60c1045550
AlessandroMaia <ale.maia.alm@gmail.com>
2020-07-15 13:46:36 -0300

    swagger security implementation

--- ErrorCenter/ErrorCenter.WebAPI/Configuration/SwaggerConfig.cs
+++ ErrorCenter/ErrorCenter.WebAPI/Configuration/SwaggerConfig.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Validations.Rules;
+using System;
 
 namespace ErrorCenter.WebAPI.Configuration
 {
@@ -7,7 +11,39 @@ namespace ErrorCenter.WebAPI.Configuration
     {
         public static IServiceCollection AddSwaggerConfig(this IServiceCollection services)
         {
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "Error Center",
+                    Version = "1.0",
+                    Description = "WEB API Central de Erros",
+                    License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+                });
+
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Insira o token JWT desta maneira: Bearer {seu token}",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
 
             return services;
         }
