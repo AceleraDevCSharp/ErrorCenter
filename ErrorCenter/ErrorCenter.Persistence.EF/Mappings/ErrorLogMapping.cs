ae14f7e049c376a55a62f223911cac10eeaba034
Gabriel Freire <gaf.freire@gmail.com>
2020-07-28 23:23:22 -0300

    ErrorLogMapping update

--- ErrorCenter/ErrorCenter.Persistence.EF/Mappings/ErrorLogMapping.cs
+++ ErrorCenter/ErrorCenter.Persistence.EF/Mappings/ErrorLogMapping.cs
@@ -4,29 +4,47 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using ErrorCenter.Persistence.EF.Models;
 using Microsoft.AspNetCore.Identity;
 
-namespace ErrorCenter.Persistence.EF.Mappings {
-  public class ErrorLogMapping : IEntityTypeConfiguration<ErrorLog> {
-    public void Configure(EntityTypeBuilder<ErrorLog> builder) {
-      builder.HasKey(x => x.Id);
+namespace ErrorCenter.Persistence.EF.Mappings
+{
+    public class ErrorLogMapping : IEntityTypeConfiguration<ErrorLog>
+    {
+        public void Configure(EntityTypeBuilder<ErrorLog> builder)
+        {
 
-      builder.Property(x => x.EnvironmentID)
-          .IsRequired();
+            builder.HasKey(e => e.Id);
 
-      builder.HasOne(x => x.Environment)
-        .WithMany("ErrorLogs")
-        .HasForeignKey(x => x.EnvironmentID);
+            builder.Property(e => e.Environment)
+                   .HasColumnType("varchar(30)")
+                   .IsRequired();
 
-      builder.Property(x => x.Level)
-          .IsRequired();
+            builder.HasOne(x => x.Environment)
+            .WithMany("ErrorLogs")
+            .HasForeignKey(x => x.EnvironmentID);
 
-      builder.Property(x => x.Title)
-          .IsRequired();
+            builder.Property(e => e.Level)
+                   .HasColumnType("varchar(30)")
+                   .IsRequired();
 
-      builder.Property(x => x.Details)
-          .IsRequired();
+            builder.Property(e => e.Title)
+                   .HasColumnType("varchar(500)")
+                   .IsRequired();
 
-      builder.Property(x => x.Origin)
-          .IsRequired();
+            builder.Property(e => e.Details)
+                   .HasColumnType("varchar(1500)")
+                   .IsRequired();
+
+            builder.Property(e => e.Origin)
+                   .HasColumnType("varchar(100)")
+                   .IsRequired();
+
+            builder.Property(e => e.CreatedAt)
+                   .HasDefaultValueSql("getdate()")
+                   .ValueGeneratedOnAdd();
+
+            builder.Property(e => e.ArquivedAt);
+
+            builder.Property(e => e.DeletedAt);
+
+        }
     }
-  }
 }
