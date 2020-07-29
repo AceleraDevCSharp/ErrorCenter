af8440c5602232f2ec18548066cb456f12578ab0
Gabriel Freire <gaf.freire@gmail.com>
2020-07-28 22:33:31 -0300

    ErrorLogViewModel update

--- ErrorCenter/ErrorCenter.WebAPI/ViewModel/ErrorLogViewModel.cs
+++ ErrorCenter/ErrorCenter.WebAPI/ViewModel/ErrorLogViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.Json.Serialization;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -12,17 +13,36 @@ namespace ErrorCenter.WebAPI.ViewModel
         public int Id { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
-        [StringLength(11, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 3)]
+        [StringLength(30, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 3)]
         public string Environment { get; set; }
 
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(30, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 3)]
         public string Level { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(500, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 5)]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(1500, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 3)]
         public string Details { get; set; }
+
+        [JsonIgnore]
         public DateTime CreatedAt { get; set; }
+
+        [JsonIgnore]
         public DateTime? ArquivedAt { get; set; }
+
+        [JsonIgnore]
         public DateTime? DeletedAt { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 3)]
         public string Origin { get; set; }
+
         public string UserEmail { get; set; }
+
         public int Quantity { get; set; }
     }
 }
